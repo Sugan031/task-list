9282b7fc9b51b0f602f1cae5180b168b8246151e
Sugan031 <suganthanv031@gmail.com>
2024-03-26 17:59:01 +0530

    updates

--- app/Http/Middleware/LoginMiddleware.php
+++ app/Http/Middleware/LoginMiddleware.php
@@ -5,52 +5,33 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Route;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Facades\Log;
 
-class LoginMiddleware
-{
+class LoginMiddleware {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle($request, Closure $next)
-    {
-        // if ( session()->has("user")) {
-            // return $next($request);
-        // }
+    public function handle($request, Closure $next) {
 
         if ($request->route()) {
             $routeName = $request->route()->getName();
         } else {
-            $routeName='user.index';
+            $routeName = 'user.index';
         }
-      
-        
+
         $excludedRoutes = [
             'user.index',
             'user.register',
             'user.create',
             'user.signin'
         ];
-        if (!session()->has('user')&& !in_array($routeName, $excludedRoutes)) {
+        if (!session()->has('user') && !in_array($routeName, $excludedRoutes)) {
             return redirect()->route('user.index');
-        } 
+        }
         return $next($request);
-    
-        //    return redirect()->route('user.index');
 
-     
-
-    
-     
-
-
-    // User is already logged in
-   
-    
     }
 }
